22d4abbe537096f03739a01cd6fd8c2ec9cb99a5
ahrooran-r <ahrooran17@gmail.com>
2021-12-31 13:19:38 +0530

    soft delete

--- src/main/java/spring/learn/jpa_hibernate/entity/basics/Course.java
+++ src/main/java/spring/learn/jpa_hibernate/entity/basics/Course.java
@@ -5,7 +5,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,6 +19,9 @@ import java.time.LocalDateTime;
 
 // following are important annotations
 @Cacheable // enables 2nd level cache
+// @SQLDelete(sql = "update course set is_deleted=true where id=?") // Hibernate only -> not JPA
+// // I can execute specific query when deleting or other such events instead of actually doing said event. This is called Soft Delete
+// @Where(clause = "isDeleted=false") // when doing a `select *`, it only retrieves non-marked rows
 @Entity
 @Table(name = "course")
 @NamedQueries(
@@ -40,6 +45,10 @@ public class Course {
 
     @UpdateTimestamp // Hibernate annotation -> there will be problems when migrating to another orm
     private LocalDateTime lastUpdated;
+
+    private boolean isDeleted;
+
+    // you cannot add default values by this method: https://stackoverflow.com/a/2554796/10582056
 
     // Workaround to use JPA instead of hibernate: https://stackoverflow.com/a/42367173/10582056
 
--- src/main/resources/data/course.sql
+++ src/main/resources/data/course.sql
@@ -6,11 +6,18 @@ create table course
     id           int          not null auto_increment,
     name         varchar(255) not null,
     primary key (id),
-    created_on   timestamp default now(),
+    is_deleted   boolean      not null default false,
+    created_on   timestamp             default now(),
     last_updated timestamp on update now()
 );
 
-insert into course(name) value ('Comp. Sci.');
+insert into course(name)
+values ('Comp. Sci.'),
+       ('Arts'),
+       ('History'),
+       ('Comp. Sci.'),
+       ('Commerce');
+
 
 select *
 from course;
